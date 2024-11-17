511e375f75b513ca0d0f7dbe1de9c62e450754e3
Ifriism <idafm2@hotmail.com>
2024-11-17 17:52:59 +0100

    Added additional cities + updated the css

--- javascript/index.js
+++ javascript/index.js
@@ -19,6 +19,36 @@ let madridTime = moment().tz("Europe/Madrid");
 madridDateElement.innerHTML = madridTime.format("MMMM Do YYYY");
 madridTimeElement.innerHTML = madridTime.format("h:mm:ss [<small>]A[</small>]");
 }
+
+let tokyoElement = document.querySelector("#tokyo");
+if (tokyoElement) {
+let tokyoDateElement = tokyoElement.querySelector(".date");
+let tokyoTimeElement = tokyoElement.querySelector(".time");
+let tokyoTime = moment().tz("Asia/Tokyo");
+
+tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+let newYorkElement = document.querySelector("#new-york");
+if (newYorkElement) {
+let newYorkDateElement = newYorkElement.querySelector(".date");
+let newYorkTimeElement = newYorkElement.querySelector(".time");
+let newYorkTime = moment().tz("America/New_York");
+
+newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+newYorkTimeElement.innerHTML = newYorkTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+let londonElement = document.querySelector("#london");
+if (londonElement) {
+let londonDateElement = londonElement.querySelector(".date");
+let londonTimeElement = londonElement.querySelector(".time");
+let londonTime = moment().tz("Europe/London");
+
+londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+}
 }
 
 
@@ -36,9 +66,9 @@ citiesElement.innerHTML = `
         <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div>
-        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
+        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(" A")}</small></div>
     </div>
-    <a href="index.html">All cities</a>`;
+    <a href="index.html">Back to all cities</a>`;
 }
 
 updateTime();
